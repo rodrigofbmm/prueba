a6eba3aa19cf13c4bd7d8e86a0f78ae5f919c4a9
rodrigofbmm <115274224+rodrigofbmm@users.noreply.github.com>
2025-05-23 12:19:18 +0200

    Update PedictPage.tsx

--- tfg_nba/front-part/islands/PedictPage.tsx
+++ tfg_nba/front-part/islands/PedictPage.tsx
@@ -57,7 +57,7 @@ export default function PredictPage() {
     const eq1Data = buildTeamData(teamLocal, 1);
     const eq2Data = buildTeamData(teamVisitor, 0);
 
-    const res = await fetch("http://localhost:8008//predecir-estadisticas", {
+    const res = await fetch("/api/predecir-estadisticas",, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ equipo1: eq1Data, equipo2: eq2Data }),
